92fa1a1b3afe3729c24a68ef4f991b31edd547f3
06000208 <52764066+06000208@users.noreply.github.com>
2021-10-15 03:46:55 -0500

    Update html & css to use new spacing

---
+++ js/ItemsService.js
@@ -0,0 +1,15 @@
+import Items from "./Items.js";
+
+/**
+ * Items class
+ */
+class ItemsService extends Items {
+    /**
+     * @param {App} app - App or derivative classes
+     */
+    constructor(app) {
+        super(app);
+    }
+}
+
+export default ItemsService;
